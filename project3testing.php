a60135f2b2ac9c3921062a74e7394b1b81803b1c
Marshall Kroske <kroskema@msu.edu>
2022-12-07 21:47:48 -0500

    testing for getcomments

--- project3testing.php
+++ project3testing.php
@@ -9,6 +9,7 @@ $user = "test";
 $pass = "test";
 $lat = "test";
 $long = "test";
+$locid = "test";
 
 $base_url = "https://webdev.cse.msu.edu/~kroskema/cse476/project3/"; // verify that this is the correct path to your web site
 $magic = "NechAtHa6RuzeR8x";
@@ -47,6 +48,12 @@ $magic = "NechAtHa6RuzeR8x";
     <p>lat: <input type="text" name="lat" value="<?php echo $lat;?>"/></p>
     <p>magic: <input type="text" name="magic" value="<?php echo $magic;?>"/></p>
     <p>lng: <input type="text" name="lng" value="<?php echo $long;?>"/></p>
+    <p><input type="submit" value="Test checkhere" /></p>
+</form>
+
+<form method="get" target="_blank" action="<?php echo $base_url; ?>gps-getcomments.php">
+    <p>locid: <input type="text" name="locId" value="<?php echo $locid;?>"/></p>
+    <p>magic: <input type="text" name="magic" value="<?php echo $magic;?>"/></p>
     <p><input type="submit" value="Test checkhere" /></p>
 </form>
 
