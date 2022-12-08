8fe6a92aec06632c6767502620579889e3c594c1
Marshall Kroske <kroskema@msu.edu>
2022-12-07 21:58:54 -0500

    changed userid to user on testing page where it was incorrect

--- project3testing.php
+++ project3testing.php
@@ -30,7 +30,7 @@ $magic = "NechAtHa6RuzeR8x";
 </form>
 
 <form method="post" target="_blank" action="<?php echo $base_url; ?>gps-createuser.php">
-    <p>Userid: <input type="text" name="user" value="<?php echo $user;?>"/></p>
+    <p>Username: <input type="text" name="user" value="<?php echo $user;?>"/></p>
     <p>magic: <input type="text" name="magic" value="<?php echo $magic;?>"/></p>
     <p>Password: <input type="text" name="pw" value="<?php echo $pass;?>"/></p>
     <p><input type="submit" value="Test create user" /></p>
@@ -48,7 +48,7 @@ $magic = "NechAtHa6RuzeR8x";
 </form>
 
 <form method="get" target="_blank" action="<?php echo $base_url; ?>gps-validateuser.php">
-    <p>Userid: <input type="text" name="user" value="<?php echo $user;?>"/></p>
+    <p>Username: <input type="text" name="user" value="<?php echo $user;?>"/></p>
     <p>magic: <input type="text" name="magic" value="<?php echo $magic;?>"/></p>
     <p>Password: <input type="text" name="pw" value="<?php echo $pass;?>"/></p>
     <p><input type="submit" value="Test validate user" /></p>
