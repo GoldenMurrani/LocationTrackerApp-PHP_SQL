3b87741a6d994c8fff5ea5dd8b3e94f760ac4212
ferrynat <ferrynat@msu.edu>
2022-12-07 21:26:00 -0500

    Update gps-getcomments.php

---
+++ gps-getcomments.php
@@ -0,0 +1,25 @@
+<?php
+require_once "db.inc.php";
+echo '<?xml version="1.0" encoding="UTF-8" ?>';
+
+if(!isset($_GET['magic']) || $_GET['magic'] != "NechAtHa6RuzeR8x") {
+    echo '<gps status="no" msg="magic" />';
+    exit;
+}
+
+process($_GET['locId']);
+
+function process($locId) {
+    $pdo = pdo_connect();
+    $query = "SELECT GpsUser.Username, GpsComment.comment FROM GpsUser, GpsComment WHERE GpsComment.location=$locId";
+    $rows = $pdo->query($query);
+    echo "<gps status=\"yes\">";
+    foreach($rows as $row ) {
+        $user = $row['Username'];
+        $comment = $row['comment'];
+
+        echo "<comment user=\"$user\" comment=\"$comment\" />\r\n";
+    }
+    echo "</gps>";
+
+}
